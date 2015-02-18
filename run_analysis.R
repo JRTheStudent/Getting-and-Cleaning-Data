9f8f8e67ad83abc55df401dc45e26d3b073514a5
J.R. Jasperson <JRTheStudent@gmail.com>
2015-02-18 12:47:46 -0700

    Extracting comments to README

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,6 @@
-## The requirements for this script provided within the course are as follows:
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation for each
-##    measurement.
-## 3. Uses descriptive activity names to name the activities in the data set.
-## 4. Appropriately labels the data set with descriptive variable names.
-## 5. From the data set in step 4, creates a second, independent tidy data set
-##    with the average of each variable for each activity and each subject.
-
-## This script creates an environment named "my" which contains three variables:
-## files: A character vector consisting of the files used in processing and 
-## output.
-## data: The data frame constructed to satisfy requirements 1-4.
-## summarizedData: The data frame constructed to satisfy requirement 5.
-## This script also outputs the "summarizedData" both to the file 
-## "summarized_data.txt" in the working directory as well as to the console.
-
-## Non-validated assumptions about the data:
-## -All files in "files" exist and are readable (see example in "Setup steps.")
-## -features.txt contains one numbered line corresponding to each variable
-##    in X_{test, train}.txt
-## -X_{test, train}.txt contain the same order and number of variables.
-## -y_{test, train}.txt contain one line corresponding to each observation
-##    in X_{test, train}.txt and each value is classified by a corresponding
-##    label in activity_labels.txt
-## -subject_{test, train}.txt contain one line corresponding to each observation
-##    in X_{test, train}.txt
-
-## Setup steps:
-## if(!(file.exists("./data") & file.info("./data")$isdir)) dir.create("./data")
-## download.file(
-##     paste(
-##          "https://d396qusza40orc.cloudfront.net/"
-##         ,"getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-##         ,sep = ""
-##     )
-##     ,destfile = "./data/data.zip"
-## )
-## write(date(), file = "./data/date_downloaded.txt")
-## unzip("./data/data.zip", exdir = "./data")
-
-## Date downloaded: Sat Feb 14 06:53:52 2015
-
+## For detailed information about this script, see the README.md within the 
+## repository at https://github.com/JRTheStudent/Getting-and-Cleaning-Data/
+## 
 ## Load libraries for use.  Note "plyr" needs to be loaded before "dplyr."
 
 library(plyr)
@@ -66,13 +26,6 @@ my$files <- c(
 )
 
 ## Requirement 1: Merge the test and training data sets.
-## The {test, train}Data data sets both contain 561 variables with 2,947 and
-## 7,352 observations, respectively.  Wrapping "read.tables" with "rbind"
-## 'stacks' the test data set atop the train data set, creating a data frame
-## with 10,299 observations of 561 variables. Combining the data sets by
-## 'stacking' in one step is more efficient but all subsequent 'stacked'
-## actions _must_ proceed by 'stacking' test data atop train data to maintain
-## coherence.
 
 my$data <- rbind(
     read.table(my$files[["testData"]])
@@ -80,19 +33,10 @@ my$data <- rbind(
 )
 
 ## Requirement 4: Label the data set with descriptive variable names.
-## The "features" files contains a numbered list of column names for the test
-## and train data sets.  Extract values from column "V2" of "features" and
-## assign to "names(my$data)."
 
 names(my$data) <- as.character(read.table(my$files[["features"]])$V2)
 
 ## Requirement 2: Extract only mean and standard deviation measurements.
-## Subselect columns with variable names containing the character strings
-## "mean" and "std" (standard deviation), case-insensitively and in any position
-## within the variable name.  As the requirements are not explicit and the
-## inclusion of (potentially) superfluous variables does not affect required
-## processing (the variables are independent in the processing) it is preferable
-## to err on the side of broader inclusivity.
 
 my$data <- my$data[, grep("mean|std", names(my$data), ignore.case = T)]
 
@@ -108,10 +52,7 @@ my$data <- mutate(my$data, subject =
                       )
 )
 
-## Append new column "activity_class" to "my$data" by repeating 'test' and
-## 'train' by the number of observations of each class as derived by the
-## respective lengths of the {test, train}Subject files.
-## TODO: factor?
+## Append new column "activity_class" to "my$data."
 
 my$data <- mutate(
     my$data
@@ -128,15 +69,6 @@ my$data <- mutate(
 )
 
 ## Requirement 3: Use descriptive activity names for the activity variable.
-## The {test, train}Activity files contain the IDs of the actvities
-## corresponding to each row of the test and train data sets, respectively. The
-## "activityLabels" file contains the labels corresponding to the activity
-## IDs within {test, train}Activity.  Joining the rbind of {test, train}Activity 
-## with "activityLabels" creates a data frame with observations corresponding to 
-## those in the "my$data" data frame with two variables, the activity ID in V1
-## and the activity label in V2. Append new column "activity" to "data" with
-## column "V2" from the merged data frame.
-## TODO: factor?
 
 my$data <- mutate(my$data, activity =
     join(
@@ -151,9 +83,6 @@ my$data <- mutate(my$data, activity =
 
 ## Requirement 5: Create independent, tidy data set with the average of each
 ## variable grouped by activity and subject.
-## Use the "dplyr" "group_by" function to aggregate the data by subject and
-## activity. Apply mean to each un-grouped column via the "dplyr"
-## "summarise_each" function.
 ## TODO: Exclude activity_class from group?
 
 my$summarizedData <- (
